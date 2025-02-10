4a5fee00339c4da3df67eab2ac1408e248a92d3a
yhbsh <houssembousmaha.ksu@gmail.com>
2025-02-10 15:07:55 +0300

    refactor list.c

--- list.c
+++ list.c
@@ -4,40 +4,37 @@
 #define COUNT 5
 #define MAX 300
 
-struct node_t {
-    struct node_t *next;
+struct ListNode {
+    struct ListNode *next;
     int data;
 };
 
 int main(void) {
-    struct node_t *head, *temp;
+    struct ListNode *head, *temp;
 
-    head = (struct node_t *)malloc(sizeof(struct node_t));
+    head = (struct ListNode *)malloc(sizeof(struct ListNode));
     head->data = rand() % MAX;
     head->next = NULL;
 
     temp = head;
 
     for (int i = 0; i < COUNT; i++) {
-        struct node_t *node = (struct node_t *)malloc(sizeof(struct node_t));
+        struct ListNode *node = (struct ListNode *)malloc(sizeof(struct ListNode));
         node->data = rand() % MAX;
         node->next = NULL;
         temp->next = node;
         temp = node;
     }
 
-    FILE *f = fopen("main.dot", "wb");
+    printf("digraph G {\n");
+    printf("    size     =\"6.6\";\n");
+    printf("    rankdir  =\"LR\";\n");
 
-    fprintf(f, "digraph G {\n");
-    fprintf(f, "\tsize     =\t\"6.6\"   ;\n");
-    fprintf(f, "\trankdir  =\t\"LR\"    ;\n");
-
-    for (struct node_t *temp = head; temp->next != NULL; temp = temp->next) {
-        fprintf(f, "\t%-7d\t->\t%-8d;\n", temp->data, temp->next->data);
+    for (struct ListNode *temp = head; temp->next != NULL; temp = temp->next) {
+        printf("    %d->%d;\n", temp->data, temp->next->data);
     }
 
-    fprintf(f, "}");
-    fclose(f);
+    printf("}\n");
 
     while (temp != NULL) {
         temp = head->next;
